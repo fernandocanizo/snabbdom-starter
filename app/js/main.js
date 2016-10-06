01ab22a0d36086e2ffe1823cb5354f8f16587bdd
Fernando L. Canizo <fernandocanizo@gmail.com>
2016-10-06 11:50:57 -0300

    Add non-working counter :(
    Counter logs decreasing seconds on the console, but doesn't update the
    view even when the same variable is being passed.
    
    WTF?

--- app/js/main.js
+++ app/js/main.js
@@ -13,11 +13,19 @@ const patch = snabbdom.init([
 
 const clockView = (currentDate) => {
   return h('div', 'Current date ' + currentDate);
-}
+};
+
+const counterView = (currentSecondsCount) => {
+  return h('div', `You have ${currentSecondsCount} seconds left`);
+};
 
 const patchClock = (oldClock, newClock) => {
   return patch(oldClock, newClock);
 }
+
+const patchCounter = (oldCounter, newCounter) => {
+  return patch(oldCounter, newCounter);
+};
 
 const updateClock = () => {
   let clockNode;
@@ -31,4 +39,27 @@ const updateClock = () => {
   };
 };
 
-setInterval(updateClock(), 1000);
+const updateCounter = () => {
+  let counterNode;
+  let seconds;
+  if ('undefined' === typeof counterNode) {
+    counterNode = document.getElementById('counter');
+  }
+
+  if ('undefined' === typeof seconds) {
+    seconds = 60;
+  }
+
+  return () => {
+    seconds -= 1;
+    console.log(`seconds: ${seconds}`);
+    return patchCounter(counterNode, counterView(seconds));
+  };
+}
+
+const clumper = () => {
+  updateClock();
+  updateCounter();
+};
+
+setInterval(updateCounter(), 1000);
